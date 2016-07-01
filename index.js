aae166fdb3e7944d81d429d1ed38d73bd3520163
Tomas Brambora <tomas.brambora@gmail.com>
2016-07-01 10:02:00 +1000

    Update index.js

--- index.js
+++ index.js
@@ -159,6 +159,7 @@
     if (false === options.dispatch) dispatch = false;
     if (false === options.decodeURLComponents) decodeURLComponents = false;
     if (false !== options.popstate) window.addEventListener('popstate', onpopstate, false);
+    if (false !== options.hashChange) window.addEventListener('hashchange', onpopstate, false);
     if (false !== options.click) {
       document.addEventListener(clickEvent, onclick, false);
     }
